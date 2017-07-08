e1e7bdb1cb159fcf448d4af6c822129856f86d6f
sxt <sxt@sxtdeiMac.local>
2017-07-08 18:02:31 +0800

    remove all files

--- application/controllers/Dashboard.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/AdminController.php';
-
-class Dashboard extends AdminController {
-    public function index() {
-        $this->layout->view('dashboard/index');
-    }
-}
-?>
--- application/helpers/acl_helper.php
+++
@@ -1,9 +0,0 @@
-<?php
-use \LeanCloud\User;
-
-function acl () {
-	if (User::getCurrentUser() != null && User::getCurrentUser()->get('username') == 'lendoo') {
-		return true;
-	}
-	return false;
-}
